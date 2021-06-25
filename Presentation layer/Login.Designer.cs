4ff91c56240aee71547aed3956aad91fc035946f
Sakib Patwary <shakibpatwary001@gmail.com>
2021-06-25 09:22:03 +0600

    Create Login.Designer.cs

---
+++ Presentation layer/Login.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace ADMS
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginGroupBox = new System.Windows.Forms.GroupBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.passWarning = new System.Windows.Forms.Label();
+            this.passTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nameWarning = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.signUpButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loginGroupBox
+            // 
+            this.loginGroupBox.Controls.Add(this.loginButton);
+            this.loginGroupBox.Controls.Add(this.passWarning);
+            this.loginGroupBox.Controls.Add(this.passTextBox);
+            this.loginGroupBox.Controls.Add(this.label6);
+            this.loginGroupBox.Controls.Add(this.nameWarning);
+            this.loginGroupBox.Controls.Add(this.nameTextBox);
+            this.loginGroupBox.Controls.Add(this.nameLabel);
+            this.loginGroupBox.Location = new System.Drawing.Point(140, 36);
+            this.loginGroupBox.Name = "loginGroupBox";
+            this.loginGroupBox.Size = new System.Drawing.Size(574, 294);
+            this.loginGroupBox.TabIndex = 3;
+            this.loginGroupBox.TabStop = false;
+            this.loginGroupBox.Text = "Log In";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(210, 198);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(216, 40);
+            this.loginButton.TabIndex = 34;
+            this.loginButton.Text = "Log In";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // passWarning
+            // 
+            this.passWarning.AutoSize = true;
+            this.passWarning.Font = new System.Drawing.Font("Maiandra GD", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passWarning.ForeColor = System.Drawing.Color.Red;
+            this.passWarning.Location = new System.Drawing.Point(219, 153);
+            this.passWarning.Name = "passWarning";
+            this.passWarning.Size = new System.Drawing.Size(0, 14);
+            this.passWarning.TabIndex = 33;
+            this.passWarning.Visible = false;
+            // 
+            // passTextBox
+            // 
+            this.passTextBox.Location = new System.Drawing.Point(210, 127);
+            this.passTextBox.Multiline = true;
+            this.passTextBox.Name = "passTextBox";
+            this.passTextBox.PasswordChar = '*';
+            this.passTextBox.Size = new System.Drawing.Size(216, 40);
+            this.passTextBox.TabIndex = 32;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(115, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 16);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Password";
+            // 
+            // nameWarning
+            // 
+            this.nameWarning.AutoSize = true;
+            this.nameWarning.Font = new System.Drawing.Font("Maiandra GD", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameWarning.ForeColor = System.Drawing.Color.Red;
+            this.nameWarning.Location = new System.Drawing.Point(219, 100);
+            this.nameWarning.Name = "nameWarning";
+            this.nameWarning.Size = new System.Drawing.Size(0, 14);
+            this.nameWarning.TabIndex = 26;
+            this.nameWarning.Visible = false;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(210, 58);
+            this.nameTextBox.Multiline = true;
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(216, 39);
+            this.nameTextBox.TabIndex = 25;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(105, 70);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(79, 16);
+            this.nameLabel.TabIndex = 24;
+            this.nameLabel.Text = "User Name";
+            // 
+            // signUpButton
+            // 
+            this.signUpButton.Font = new System.Drawing.Font("Maiandra GD", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signUpButton.Location = new System.Drawing.Point(491, 378);
+            this.signUpButton.Name = "signUpButton";
+            this.signUpButton.Size = new System.Drawing.Size(75, 23);
+            this.signUpButton.TabIndex = 5;
+            this.signUpButton.Text = "Sign Up";
+            this.signUpButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(284, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Do Not Have An Account?";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 435);
+            this.Controls.Add(this.loginGroupBox);
+            this.Controls.Add(this.signUpButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.loginGroupBox.ResumeLayout(false);
+            this.loginGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox loginGroupBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label passWarning;
+        public System.Windows.Forms.TextBox passTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label nameWarning;
+        public System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button signUpButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
